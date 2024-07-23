9beb0c8219674af5cf7e476ea6ba1ab2a776f277
Sunil Ping <spingkp@gmail.com>
2024-07-23 18:03:27 +0530

    Initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+           <div className="bg-blue-300 h-14 w-14 justify-between text-center pt-6">
+            Sunil
+            </div>
+  );
+}
